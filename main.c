5e2635909a54f644e2a29c3ddddb966e96a55c59
Bruce Junior Allain <ballain@c1r1s1.42antananarivo.mg>
2024-02-20 19:48:56 +0300

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ballain <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 17:59:32 by ballain           #+#    #+#             */
-/*   Updated: 2024/02/20 16:35:48 by ballain          ###   ########.fr       */
+/*   Updated: 2024/02/20 19:33:04 by ballain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,15 @@
 
 int	main(void)
 {
-	char	*str_1;
-	char	*str_2;
-	int		i;
+	char	str_1[19] = "alightechs";
+	char	str_2[10];
+	char	str_3[4];
 
-	i = 0;
-	str_1 = strdup("Hello");
-	str_2 = ft_strdup(" world");
+	//i = strlcpy(str_2, str_1, 2);
+	memmove(str_1 + 5, str_1, 14);
+//	printf("ORIGINAL : %s, %zu\n", str_1, i);
+//	printf("COPY     : %s\n", str_2);
 	printf("ORIGINAL : %s\n", str_1);
-	printf("COPY     : %s\n", str_2);
-	free(str_1);
-	free(str_2);
+//	printf("COPY     : %s\n", str_2);
 	return (0);
 }
