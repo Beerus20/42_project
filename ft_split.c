00b3e5e2ff8289a9f744c4ce592e4627c51ec34d
Beerus27 <brucejuniorallain@gmail.com>
2024-02-22 22:35:56 +0300

    ajustement des headers

--- ft_split.c
+++ ft_split.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
+#include "libft.h"
 
 static int	ft_count_char(const char *s, const char c)
 {
